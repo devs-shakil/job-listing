a53459c635bbcfecaf66440a0ebe7be12366cf7b
shakilwebmaster <mdshakil102036@gmail.com>
2022-02-21 23:48:59 +0600

    job listing todo app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import FirstComponent from "./Components/FirstComponent";
+const App = () =>{
+  return(
+    <>
+      <FirstComponent name= "Shakil" imgSrc=""></FirstComponent>
+      <FirstComponent name= "Suhad"></FirstComponent>
+      <FirstComponent name= "Rakib "></FirstComponent>
+      <FirstComponent name= "Sanzida Magi"></FirstComponent>
+      <FirstComponent name= "Rubi"></FirstComponent>
+    </>
+  )
+}
+
+
+
+
+export default App;
---
+++ src/Components/FirstComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FirstComponent = (props) =>{
+    return (
+        <>
+            <h3>Hi my name is {props.name}</h3>
+            
+
+        </>
+    )
+}
+
+export default FirstComponent;
